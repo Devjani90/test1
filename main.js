de118e408eb0af5a2aaee3c50bfb617b6e00c676
Devjani90 <debajani770@gmail.com>
2024-07-15 10:23:25 +0530

    Feat-1: Testing purpose

---
+++ main.js
@@ -0,0 +1,61 @@
+const menus3 = document.querySelectorAll('.menus3');
+menus3.forEach(item => {
+    item.innerHTML =   `
+        <li>
+            <i class="bi bi-alexa"></i>
+            <p>Icons</p>
+        </li>
+        <li>
+            <i class="bi bi-amd"></i>
+            <p>Amazing views</p>
+        </li>
+        <li>
+            <i class="bi bi-airplane-engines-fill"></i>
+            <p>Amazing pools</p>
+        </li>
+        <li>
+            <i class="bi bi-alarm"></i>
+            <p>Farms</p>
+        </li>
+        <li>
+            <i class="bi bi-android"></i>
+            <p>Bed &Breakfast</p>
+        </li>
+        <li>
+            <i class="bi bi-amazon"></i>
+            <p>Golfing</p>
+        </li>
+        <li>
+            <i class="bi bi-arrow-down-circle-fill"></i>
+            <p>LakeFront</p>
+        </li>
+        <li>
+            <i class="bi bi-apple"></i>
+            <p>OMG</p>
+        </li>
+        <li>
+            <i class="bi bi-arrow-repeat"></i>
+            <p>Countryside</p>
+        </li>
+        <li>
+            <i class="bi bi-arrows-fullscreen"></i>
+            <p>Luxe</p>
+        </li>
+        <li>
+            <i class="bi bi-backpack2"></i>
+            <p>Beachfront</p>
+        </li>
+        <li>
+            <i class="bi bi-balloon"></i>
+            <p>Cabins</p>
+        </li>
+        <li>
+            <i class="bi bi-bank2"></i>
+            <p>Treehouse</p>
+        </li>
+        <li>
+            <i class="bi bi-binoculars"></i>
+            <p>Castles</p>
+        </li>
+`
+})
